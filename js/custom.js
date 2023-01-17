009df34058b69270366a09823d5b4c1e064769dd
agnunga <agunga3d@gmail.com>
2023-01-17 10:25:49 +0300

    show about us description

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+
+// $(".description-section").click(function(){
+//     $('.design-description').addClass('d-block').removeClass('d-none');
+//     $('.design-image').addClass('d-none').removeClass('d-block');
+// });
+
+function show_description(){
+    $('.design-description').addClass('d-block').removeClass('d-none');
+    $('.design-image').addClass('d-none').removeClass('d-block');
+}
