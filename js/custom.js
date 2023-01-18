24d7eb2f93fb40dcb64fc1b6bb18c6523833b54c
agnunga <agunga3d@gmail.com>
2023-01-18 17:52:42 +0300

    hover effect polished

--- js/custom.js
+++ js/custom.js
@@ -4,9 +4,34 @@
 //     $('.design-image').addClass('d-none').removeClass('d-block');
 // });
 
-function show_description(){
-    $('.design-description').addClass('d-block').removeClass('d-none');
-    $('.design-image').addClass('d-none').removeClass('d-block');
+function show_description(a){
+    if(a == 1){
+        if ($('p.d-none').length) {
+            $('.design-description1').addClass('d-block').removeClass('d-none');
+            $('.design-image1').addClass('d-none').removeClass('d-block');
+        }else{
+            $('.design-description1').addClass('d-none').removeClass('d-block');
+            $('.design-image1').addClass('d-block').removeClass('d-none');
+        }
+    }
+    else if (a == 2){
+        if ($('p.d-none').length) {
+            $('.design-description2').addClass('d-block').removeClass('d-none');
+            $('.design-image2').addClass('d-none').removeClass('d-block');
+        }else{
+            $('.design-description2').addClass('d-none').removeClass('d-block');
+            $('.design-image2').addClass('d-block').removeClass('d-none');
+        }
+    }
+    else if (a == 3){
+        if ($('p.d-none').length) {
+            $('.design-description3').addClass('d-block').removeClass('d-none');
+            $('.design-image3').addClass('d-none').removeClass('d-block');
+        }else{
+            $('.design-description3').addClass('d-none').removeClass('d-block');
+            $('.design-image3').addClass('d-block').removeClass('d-none');
+        }
+    }
 }
 
 
