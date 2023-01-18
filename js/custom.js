6e2b69b2f4ad64337c166518fb96e1f0908411f4
agnunga <agunga3d@gmail.com>
2023-01-18 10:32:01 +0300

    portfolio hover effect added

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,19 @@ function show_description(){
     $('.design-description').addClass('d-block').removeClass('d-none');
     $('.design-image').addClass('d-none').removeClass('d-block');
 }
+
+
+$(document).ready(function () {
+    $('#wb_Image3 img').hover(function () {
+        $(this).stop().animate({
+            opacity: .4
+        }, 10);
+        $('.portfolio-text').removeClass('hide');
+    }, function () {
+        $(this).stop().animate({
+            opacity: 1
+        }, 500);
+        $('.portfolio-text').addClass('hide');
+    });
+});
+
